158c08d6c6af26151e549594c5820e32b9c1218a
Marcos <Marcos@Profesor>
2024-03-11 10:02:53 +0100

    Elefante implmenta Peleador

--- src/animales/Elefante.java
+++ src/animales/Elefante.java
@@ -1,14 +1,19 @@
 package animales;
 
-public class Elefante extends Animal {
+import luchadores.Peleador;
+
+public class Elefante extends Animal
+ 		implements Peleador {
 
 	private int edad;
+	private int salud;
 
 	
 	public Elefante(String nombre, int edad, int peso) {
 		
 		super(nombre,peso);
 		setEdad(edad);
+		salud = 100;
 	}
 
 	
@@ -58,6 +63,34 @@ public class Elefante extends Animal {
 			case 6,7,8: 	return 8.06f;
 			default: 		return 5.97f;
 		}
+	}
+
+
+
+	@Override
+	public int ataque() {
+		if(this.getPeso()<1000) {
+			return 75;
+		}
+		else return 100;
+	}
+	@Override
+	public int defensa() {
+		if(this.getPeso()<100) {
+			return 50;
+		}
+		else if(this.getPeso()<500) {
+			return 75;
+		}
+		else return 100;
+	}
+	@Override
+	public int puntosVida() {
+		return salud;
+	}
+	@Override
+	public void danio(int pupa) {
+		salud -= pupa;
 	}
 	
 	
